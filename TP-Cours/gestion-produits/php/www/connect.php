410171e13381275e1fa79921f0cd372848d7d9c0
Etienne <etiennewazer@gmail.com>
2025-06-06 11:02:02 +0200

    Mise à jour de la configuration Docker et des paramètres de connexion à la base de données avec des variables d'environnement.

--- TP-Cours/gestion-produits/php/www/connect.php
+++ TP-Cours/gestion-produits/php/www/connect.php
@@ -1,11 +1,12 @@
 <?php
-    $host = "db";
-    $username = "root";
-    $password = "root";
-    $db = "gestion_produits";
+    $driver = getenv("DB_DRIVER") ?: "mysql";
+    $host = getenv("DB_HOST") ?: "db";
+    $username = getenv("DB_USERNAME") ?: "root";
+    $password = getenv("DB_PASSWORD") ?: "root";
+    $db = getenv("DB_NAME") ?: "gestion_produits";
 
     // Connexion avec pdo mysql
-    $db = new PDO("mysql:host=$host;dbname=$db", $username, $password);
+    $db = new PDO("$driver:host=$host;dbname=$db", $username, $password);
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
     
